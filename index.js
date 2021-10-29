ec00424b8cf4cac554bdb36a41a929fced76c786
Susi <susl@rickroll.link>
2021-10-29 12:35:38 +0200

    more routing

--- index.js
+++ index.js
@@ -13,13 +13,6 @@ const server = require("./lib/server.js");
 app.use(middleware);
 app.use(server);
 
-app.all("*", (req, res) => {
-  // in case that the response wasnt ended by the server, notify the user
-  res
-    .status(500)
-    .end("There has been a serverside Error that could not be handled");
-});
-
 app.listen(PORT, () => {
   console.log("App listening on Port", PORT);
 });
