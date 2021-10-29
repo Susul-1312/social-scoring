876ee2648aa79915fd626104564905be8360a459
Susi <susl@rickroll.link>
2021-10-29 10:25:24 +0200

    add first routing capabilities

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// the middleware is used for auth and stuff
+const middleware = require("./lib/middleware.js");
+
+// the server then actually returns the pages
+const server = require("./lib/server.js");
+
+app.use(middleware);
+app.use(server);
+
+app.all((req, res) => {
+  // in case that the response wasnt ended by the server, notify the user
+  res
+    .send(500)
+    .end("There has been a serverside Error that could not be handled");
+});
 
 app.listen(PORT, () => {
   console.log("App listening on Port", PORT);
-})
+});
